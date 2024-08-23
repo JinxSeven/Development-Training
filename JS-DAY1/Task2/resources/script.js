282cc0f5295dbd0d81a3d080258f8dba58114d36
jinxseven <roadrustersam@gmail.com>
2024-08-23 15:17:27 +0530

    js-d1t2-update

--- JS-DAY1/Task2/resources/script.js
+++ JS-DAY1/Task2/resources/script.js
@@ -5,11 +5,28 @@ let taskList = document.querySelector('ul');
 todoButton.addEventListener('click', function() {
     let taskItem = document.createElement('li');
     let taskCheck = document.createElement('input');
+    let taskButton = document.createElement('button');
+    taskButton.innerText = 'Delete';
+    taskButton.setAttribute('class', 'delete-button')
 
     taskCheck.setAttribute('type', 'checkbox');
-    taskItem.appendChild(taskCheck);
-    taskItem.innerText = taskInput.value;
+    taskCheck.setAttribute('class', 'check-box');
+    taskItem.append(taskCheck);
+    taskItem.append(taskInput.value);
+    taskItem.append(taskButton);
 
     taskList.append(taskItem);
-    console.log(taskList.innerHTML);
+    taskCheck.addEventListener('changed', function() {
+        if (taskCheck.checked) {
+            taskItem.style.textDecoration = 'line-through';
+        }else{
+            taskItem.style.textDecoration = 'none';
+        }
+    })
+})
+
+let deleteButton = document.getElementsByClassName('delete-button')
+
+deleteButton.addEventListener('click', function deleteTask() {
+    
 })
