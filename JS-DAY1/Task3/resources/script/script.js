de2ea05b6a66d4098827bd2ef2f199d392fed612
Samuel Livingston <roadrustersam@gmail.com>
2024-08-23 23:37:15 +0530

    js-d1t3-initial-steps

---
+++ JS-DAY1/Task3/resources/script/script.js
@@ -0,0 +1,5 @@
+let calculator = docment.querySelector('.form-container');
+let studInput = document.getElementById('stud-name-input');
+let gradeInput = document.getElementById('grade-input');
+
+calculator.addEventListener()
