6bcb2fd2298749958c63f453ca21dee404a5c6bb
jinxseven <roadrustersam@gmail.com>
2024-08-24 14:09:32 +0530

    js-d1t3-complete

--- JS-DAY1/Task1/resources/script/script.js
+++ JS-DAY1/Task1/resources/script/script.js
@@ -2,7 +2,31 @@ let calculator = document.querySelector('.calculator');
 let answer = document.getElementById('interest');
 let total = document.getElementById('total-amount');
 let addInfo = document.getElementById('add-info');
+let principalInput = document.getElementById('principal');
+let roiInput = document.getElementById('rate-of-interest');
+let timeInput = document.getElementById('time');
 let interest = 0;
+
+principalInput.addEventListener('blur', function() {
+    if (principal < 500 || principal > 10000 || isNaN(principal)) {
+        alert("Invalid principal input rangecor type!")
+            principalInput.value= '';
+    }
+})
+
+roiInput.addEventListener('blur', function() {
+    if (isNaN(rateOfInterest)) {
+        alert("Invalid rate 0f interest input type!")
+        roiInput.value= '';
+    }
+})
+
+timeInput.addEventListener('blur', function() {
+    if (isNaN(time)) {
+        alert("Invalid time input type!")
+        timeInput.value= '';
+    }
+})
 
 calculator.addEventListener("submit", function(def) {
     def.preventDefault();
@@ -10,16 +34,6 @@ calculator.addEventListener("submit", function(def) {
     let principal = parseFloat(document.getElementById('principal').value);
     let rateOfInterest = parseFloat(document.getElementById('rate-of-interest').value);
     let time = parseFloat(document.getElementById('time').value);
-
-    if (isNaN(principal) || isNaN(rateOfInterest) || isNaN(time)) {
-        alert("Invalid Input Type!")
-        calculator.reset();
-    }
-
-    if (principal < 500 || principal > 10000) {
-        alert("Invalid Input Range!")
-            calculator.reset();
-    }
 
     if (principal < 1000 && rateOfInterest < 5) rateOfInterest = 5;
     if (principal < 5000 && rateOfInterest < 7) rateOfInterest = 7;
@@ -36,4 +50,4 @@ calculator.addEventListener("submit", function(def) {
     answer.textContent = interest.toFixed(2);
     total.textContent = (principal + interest).toFixed(2);
     calculator.reset();
-});
+})
--- JS-DAY1/Task3/resources/script/script.js
+++ JS-DAY1/Task3/resources/script/script.js
@@ -1,5 +1,86 @@
-let calculator = docment.querySelector('.form-container');
+let studForm = document.querySelector('.form-container');
 let studInput = document.getElementById('stud-name-input');
 let gradeInput = document.getElementById('grade-input');
+let addStudBtn = document.getElementById('add-stud-btn');
+let dispGradeBtn = document.getElementById('disp-grade-btn');
+let calcAvgBtn = document.getElementById('calc-avg-btn');
+let gradeList = document.getElementById('grades-list');
+let avgOut = document.getElementById('avg-out');
 
-calculator.addEventListener()
+class Student {
+    constructor(name, grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+    getName() {
+        return this.name;
+    }
+    getGrade() {
+        return this.grade;
+    }
+}
+
+let arrStuds = [];
+let listCount = 0;
+
+studInput.addEventListener('blur', function() {
+    if (studInput.value == '' || !isNaN(studInput.value)) { 
+        alert('Name cannot be empty or a number!');
+        studInput.style.borderColor = '#ff474c';
+    } else {
+        studInput.style.borderColor = '#90ee90';
+    }
+})
+
+gradeInput.addEventListener('blur', function() {
+    if (gradeInput.value == '' || isNaN(gradeInput.value)) {
+        alert('Grade cannot be empty or invalid type!');
+        gradeInput.style.borderColor = '#ff474c';
+    } else {
+        gradeInput.style.borderColor = '#90ee90';
+    }
+})
+
+addStudBtn.addEventListener('click', function(def) {
+    def.preventDefault();
+    
+    const newStud = new Student(studInput.value, gradeInput.value);
+    arrStuds.push(newStud); 
+    console.log(arrStuds.length);
+    if (listCount < arrStuds.length) {
+        for (let x = listCount; x < arrStuds.length; x++) {
+            let listItem = document.createElement('li');
+            let studentData = arrStuds[x].getName() + ' - Grade: ' + arrStuds[x].getGrade();
+            let data = document.createTextNode(studentData);
+            listItem.append(data)
+            console.log(arrStuds[x]);
+            gradeList.append(listItem);
+            listCount++;
+        }
+    }
+})
+
+let toggle = 0;
+
+dispGradeBtn.addEventListener('click', function(def){
+    def.preventDefault();
+    let showList = document.querySelector('ol');
+    if (toggle == 0) {
+        showList.style.display = 'block';
+        toggle = (toggle + 1) % 2;
+    } else {
+        showList.style.display = 'none';
+        toggle = (toggle + 1) % 2;
+    }
+})
+
+calcAvgBtn.addEventListener('click', function() {
+    let avg = 0;
+    for (let x = 0; x < arrStuds.length; x++) {
+        avg += parseFloat(arrStuds[x].getGrade());
+    }
+
+    avg = avg / arrStuds.length;
+    console.log(avg);
+    avgOut.textContent = avg.toFixed(2);
+})
