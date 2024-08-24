ce216147d93a58ccdc9f9a8cdf8edadea0e48d32
jinxseven <roadrustersam@gmail.com>
2024-08-24 14:09:55 +0530

    js-d2t1-initialized

---
+++ JS-DAY2/Task1/resources/script/script.js
@@ -0,0 +1,6 @@
+const prodNameInp = document.getElementById('prod-name-inp');
+const prodPriceInp = document.getElementById('prod-price-inp');
+const prodQuantInp = document.getElementById('prod-quant-inp');
+const addProdBtn = document.getElementById('add-prod-btn');
+
+
