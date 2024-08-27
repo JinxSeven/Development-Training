4fb52bfcfa2ead0d62c20bbb7beff162bf26f1a3
jinxseven <roadrustersam@gmail.com>
2024-08-27 16:29:09 +0530

    js-d1t2-editable-tasks

--- JS-DAY1/Task2/resources/script/script.js
+++ JS-DAY1/Task2/resources/script/script.js
@@ -17,7 +17,7 @@ todoButton.addEventListener('click', function() {
     let taskItem = document.createElement('li');
     let taskCheck = document.createElement('input');
     let deleteButton = document.createElement('button');
-    let p = document.createElement('p');
+    let taskName = document.createElement('input');
     
     if (taskInput.value == '') {
         document.getElementById('task-err').style.opacity = '1';
@@ -28,28 +28,42 @@ todoButton.addEventListener('click', function() {
         taskInput.style.borderColor = '#d8d8d8';
     }
 
-    p.innerText = taskInput.value;
+    taskName.value = taskInput.value;
+    taskInput.value = '';
     deleteButton.innerText = 'Delete';
     deleteButton.setAttribute('class', 'delete-button')
+    taskName.setAttribute('id', 'task-edit');
+    taskName.setAttribute('placeholder', "Task can't be empty!");
 
     taskCheck.setAttribute('type', 'checkbox');
     taskCheck.setAttribute('class', 'check-box');
 
     taskItem.append(taskCheck);
-    taskItem.append(p);
+    taskItem.append(taskName);
     taskItem.append(deleteButton);
 
     taskList.append(taskItem);
 
     taskCheck.addEventListener('change', function() {
         if (taskCheck.checked) {
-            p.style.textDecoration = 'line-through';
+            taskName.style.textDecoration = 'line-through';
         } else {
-            p.style.textDecoration = 'none';
+            taskName.style.textDecoration = 'none';
         }
     })
 
     deleteButton.addEventListener('click', function() {
         taskList.removeChild(taskItem);
     })
+
+
+    taskName.onblur = function taskRename() {
+        if (taskName.value == '') {
+            taskCheck.disabled = true;
+        } else {
+            taskCheck.disabled = false;
+        }
+    }
 })
+
+
