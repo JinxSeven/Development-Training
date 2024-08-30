49195b23634772286e54201bbe564ff40da32590
jinxseven <roadrustersam@gmail.com>
2024-08-30 19:48:09 +0530

    JS-D2T2-Weather-Update

--- JS-DAY2/Task2/resources/script/script.js
+++ JS-DAY2/Task2/resources/script/script.js
@@ -4,11 +4,16 @@ const nameOut = document.getElementById('name-out');
 const tempOut = document.getElementById('temp-out');
 const humidOut = document.getElementById('humid-out');
 
+let humidity;
+let temperature;
+let cityName;
+
 searchBtn.addEventListener('click', async function() {
     const city = searchInp.value;
     searchInp.value = '';
     if (city == '') {
-        console.log('City name cant be empty!');
+        const cityError = document.getElementById('city-err');
+        cityError.innerText = "City name can't be empty!";
         return;
     }
     const apiKey = 'e124fb640b814c031daa1aabde059d48';
@@ -17,13 +22,20 @@ searchBtn.addEventListener('click', async function() {
     try {
         const res = await fetch(apiUrl);
         const apiData = await res.json();
-        const humidity = apiData.main.humidity;
-        const temprature = ((apiData.main.temp - 32) * 5 / 9).toFixed(2);
-        const cityName = apiData.name;
-        nameOut.innerText = cityName;
-        tempOut.innerText = temprature;
-        humidOut.innerText = humidity;
+
+        console.log(apiData);
+        if (!res.ok) {
+            throw new Error(`${apiData.cod}: ${apiData.message}`);
+            
+        } else {
+            humidity = apiData.main.humidity;
+            temperature = ((apiData.main.temp) - 273.15).toFixed(2);
+            cityName = apiData.name;
+            nameOut.innerText = cityName;
+            humidOut.innerText = humidity;
+            tempOut.innerText = temperature;
+        }
     } catch (error) {
-        console.error(error);
+        console.error(error.message);
     }
 })
