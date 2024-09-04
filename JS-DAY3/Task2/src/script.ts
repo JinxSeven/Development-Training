e7a7f2b1bd47face337a8f6cb65b7ebdcb406dbb
Samuel Livingston <admin@SYSLP693.claysys.com>
2024-09-04 12:33:25 +0530

    JS-D3T2-Style-Update

--- JS-DAY3/Task2/src/script.ts
+++ JS-DAY3/Task2/src/script.ts
@@ -2,3 +2,6 @@ const nameInput = document.getElementById('name-input') as HTMLInputElement;
 const emailInput = document.getElementById('email-input') as HTMLInputElement;
 const roleInput = document.getElementById('role-select') as HTMLSelectElement;
 const addUserButton = document.getElementById('add-user-btn') as HTMLButtonElement;
+const mainList = document.getElementById('main-list') as HTMLOListElement;
+
+
