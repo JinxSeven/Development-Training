309f4515d7bdfcfdbec444913a95098641f99c24
Samuel Livingston <roadrustersam@gmail.com>
2024-09-19 08:14:25 +0530

    NGD1-T1-CRUD-Update

--- NG-DAY1/Task1/src/app/todo-list/todo-list.component.ts
+++ NG-DAY1/Task1/src/app/todo-list/todo-list.component.ts
@@ -10,23 +10,47 @@ import { Task } from '../shared/interface.task';
 
 export class TodoListComponent {
     taskData: Task[] = [];
+    editMode = false;
+    taskToEditIndx: number | null = null;
+    taskToEdit: Task = {
+        taskName: '',
+        userName: '',
+        taskStatus: false
+    }
 
     addNewTask(taskForm: NgForm) {
+        if (this.editMode) {
+            this.taskData[this.taskToEditIndx!] = {
+                taskName: taskForm.controls['taskName'].value,
+                userName: taskForm.controls['userName'].value,
+                taskStatus: false
+            }
+            this.editMode = !this.editMode;
+            this.taskToEditIndx = null;
+            taskForm.reset();
+            return;
+        }
         const taskObj: Task = {
-            taskName: taskForm.controls['taskName'].value,
-            userName: taskForm.controls['userName'].value,
-            taskStatus: false
+        taskName: taskForm.controls['taskName'].value,
+        userName: taskForm.controls['userName'].value,
+        taskStatus: false
         }
         this.taskData.push(taskObj);
         taskForm.reset();
     }
 
-    onDeleteTask(index: number) {
-        this.taskData.splice(index, 1);
-    }
-
     onStatusChange(index: number) {
         const task = this.taskData[index];
         console.log(task);
+    }
+
+    onEditTask(index: number) {
+        this.editMode = !this.editMode;
+        this.taskToEdit = this.taskData[index];
+        this.taskToEditIndx = index;
+    }
+
+    onDeleteTask(index: number) {
+        this.taskData.splice(index, 1);
     }
 }
