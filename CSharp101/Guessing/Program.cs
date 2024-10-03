d9a0a04c1cfd73c07985dfa760134df0feeabe9d
Samuel Livingston <john247samuel@gmail.com>
2024-10-03 18:07:22 +0530

    Git-Repo-Refactored+CS101

---
+++ CSharp101/Guessing/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rand = new Random();
+int guessTheNumber = rand.Next(1, 100);
+int userGuess = -1;
+
+while (userGuess != guessTheNumber)
+{
+    Console.Write("Guess a number between 1 - 100: ");
+    string userInput = Console.ReadLine();
+
+    try
+    {
+        userGuess = int.Parse(userInput);
+        if (userGuess < guessTheNumber) Console.WriteLine("Too Low, Guess again!");
+        else if (userGuess > guessTheNumber) Console.WriteLine("Too High, Guess again!");
+    }
+    catch (FormatException) 
+    {
+        Console.WriteLine("Invalid input type, Try again!");
+    }
+}
+
+Console.WriteLine($"You guessed it right! - {userGuess}");
