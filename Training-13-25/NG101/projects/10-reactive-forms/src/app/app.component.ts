92453ad84ea69874693dc2146af1a626956e2668
Samuel Livingston <john247samuel@gmail.com>
2024-11-30 18:32:13 +0530

    08&09-NG101

--- Training-13-25/NG101/projects/10-reactive-forms/src/app/app.component.ts
+++ Training-13-25/NG101/projects/10-reactive-forms/src/app/app.component.ts
@@ -1,31 +1,58 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+    FormControl,
+    FormControlName,
+    FormGroup,
+    ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [ReactiveFormsModule],
-  template: `
-    <article>
-      <h1>Blog Post</h1>
-      <form name="blogForm">
-        <section>
-          <label for="title">Post Title</label>
-          <input type="text" id="title" />
+    selector: 'app-root',
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    template: `
+        <article>
+            <h1>Blog Post</h1>
+            <form
+                name="blogForm"
+                [formGroup]="blogForm"
+                (ngSubmit)="handleFormSubmit()"
+            >
+                <section>
+                    <label for="title">Post Title</label>
+                    <input type="text" id="title" formControlName="blogTitle" />
 
-          <label for="body">Post Body</label>
-          <textarea name="" id="body" cols="30" rows="10"></textarea>
-        </section>
-        <button type="submit">Submit Post</button>
-      </form>
-    </article>
-  `,
-  styles: [],
+                    <label for="body">Post Body</label>
+                    <textarea
+                        name=""
+                        id="body"
+                        cols="30"
+                        rows="10"
+                        formControlName="blogBody"
+                    ></textarea>
+                </section>
+                <button type="submit">Submit Post</button>
+            </form>
+        </article>
+    `,
+    styles: [],
 })
 export class AppComponent {
-  handleFormSubmit() {}
+    blogForm = new FormGroup({
+        blogTitle: new FormControl(''),
+        blogBody: new FormControl(''),
+    });
+    handleFormSubmit() {
+        this.postBlog(
+            this.blogForm.value.blogTitle,
+            this.blogForm.value.blogBody
+        );
+    }
 
-  postBlog(title: string | null | undefined, body: string | null | undefined) {
-    console.log(`Posting blog titles ${title}, with the contents ${body}.`);
-  }
+    postBlog(
+        title: string | null | undefined,
+        body: string | null | undefined
+    ) {
+        console.log(`Posting blog titles ${title}, with the contents ${body}.`);
+    }
 }
