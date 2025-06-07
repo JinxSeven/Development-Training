7ee39286a1da8c45d6c1e29d8b20df840e1f01e3
Samuel Livingston <jinxseven@users.noreply.github.com>
2025-06-07 16:49:14 +0530

    Redux - Wishlist Update

--- React101/react-shop/src/redux-store.js
+++ React101/react-shop/src/redux-store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './features/cart/cartManager'
+import wishlistReducer from './features/wishlist/wishlistManager'
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer
+    cart: cartReducer,
+    wishlist: wishlistReducer
   }
 });
 
