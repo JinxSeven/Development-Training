bc4fab78169861ecf26945f4cd21302a03bd64c7
Samuel Livingston <jinxseven@users.noreply.github.com>
2025-10-20 14:49:35 +0530

    Update page.tsx

--- React-Training/nextjs-react/knowledge-base/src/app/login/page.tsx
+++ React-Training/nextjs-react/knowledge-base/src/app/login/page.tsx
@@ -1,9 +1,52 @@
+"use client";
+
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { UserLoginRequest, FormFieldValidations } from "../interfaces/user";
+import { useEffect, useMemo, useState } from "react";
+import { authLogin } from "../api/userService";
 
 export default function Login() {
+  const [errors, setErrors] = useState<FormFieldValidations>({});
+  const disableSubmit = useMemo(() => {
+    return errors.username !== null || errors.password !== null;
+  }, [errors.username, errors.password]);
+
+  const validateField = (e: React.FocusEvent<HTMLInputElement>) => {
+    const fieldValidations: FormFieldValidations = {};
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "username":
+        if (!/^(?!.*(__|--|\.\.))(?=(.*[a-z]){3})[a-z0-9_.-]+$/.test(value)) {
+          fieldValidations.username = "Username not valid";
+        } else fieldValidations.username = null;
+        break;
+      case "password":
+        if (value.trim() === "") {
+          fieldValidations.password = "Password cannot be empty";
+        } else fieldValidations.password = null;
+        break;
+    }
+
+    setErrors((prev) => ({
+      ...prev,
+      [name]: fieldValidations[name as keyof FormFieldValidations],
+    }));
+  };
+
+  async function onSignIn(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const loginCred: UserLoginRequest = {
+      username: e.target.username.value,
+      password: e.target.password.value
+    }
+
+    authLogin(loginCred)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div>
@@ -12,38 +55,60 @@ export default function Login() {
         </h2>
       </div>
       <div className="mt-10 sm:mx-6 sm:w-full sm:max-w-sm">
-        <form action="#" method="POST" className="space-y-6">
+        <form onSubmit={onSignIn} method="POST" className="space-y-6">
           <div>
-            <Label htmlFor="username" className="pl-1 font-thin text-base">Username</Label>
+            <div className="flex items-center justify-between">
+              <Label htmlFor="username" className="px-1 font-thin text-base">
+                Username
+              </Label>
+              {errors.username && (
+                <div className="text-sm text-red-700">{errors.username}</div>
+              )}
+            </div>
             <div className="mt-2">
               <Input
                 id="username"
                 name="username"
                 type="text"
-                required
                 autoComplete="username"
+                onChange={validateField}
+                onBlur={validateField}
               />
             </div>
           </div>
 
           <div>
             <div className="flex items-center justify-between">
-              <Label htmlFor="password" className="pl-1 font-thin text-base">Password</Label>
-              <div className="text-sm"></div>
+              <Label htmlFor="password" className="px-1 font-thin text-base">
+                Password
+              </Label>
+              {errors.password && (
+                <div className="text-sm text-red-700">{errors.password}</div>
+              )}
             </div>
             <div className="mt-2">
               <Input
                 id="password"
                 name="password"
                 type="password"
-                required
+                // required
                 autoComplete="current-password"
+                onChange={validateField}
+                onBlur={validateField}
+                min={8}
               />
             </div>
           </div>
 
           <div>
-            <Button variant="outline" className="text-base">Sign In</Button>
+            <Button
+              variant="outline"
+              className="text-base"
+              type="submit"
+              disabled={disableSubmit}
+            >
+              Sign In
+            </Button>
           </div>
         </form>
 
