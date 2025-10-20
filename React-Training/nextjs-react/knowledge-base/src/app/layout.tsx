37de83e6e6f8c84e1e97a9dfcbe6422b24d60cd3
Samuel Livingston <jinxseven@users.noreply.github.com>
2025-10-20 18:57:21 +0530

    Update layout.tsx

--- React-Training/nextjs-react/knowledge-base/src/app/layout.tsx
+++ React-Training/nextjs-react/knowledge-base/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// @ts-ignore
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
@@ -36,7 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         ></ThemeProvider>
         {children}
-        <Toaster />
+        <Toaster toastOptions={{ className: "font-sans" }}/>
       </body>
     </html>
   );
