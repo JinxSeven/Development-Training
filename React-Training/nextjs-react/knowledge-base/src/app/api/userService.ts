167857ce8e9cac8c17ab9fc2752f76b64d69a4d1
Samuel Livingston <jinxseven@users.noreply.github.com>
2025-10-20 18:57:14 +0530

    Create userService.ts

---
+++ React-Training/nextjs-react/knowledge-base/src/app/api/userService.ts
@@ -0,0 +1,32 @@
+import { UserLoginRequest, UserLoginResponse } from "../interfaces/user";
+import { setUserLocalStorage } from "../lib/localStorageUtils";
+
+export async function authLogin(cred: UserLoginRequest): Promise<UserLoginResponse> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/${process.env.NEXT_PUBLIC_AUTH_CONTROLLER_NAME}/TokenAuth`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cred),
+      }
+    );
+
+    if (!res.ok) {
+      const errorText = await res.text();
+      throw new Error(
+        `Request failed (${res.status}): ${errorText || res.statusText}`
+      );
+    }
+
+    const data = await res.json() 
+    setUserLocalStorage(data)
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
