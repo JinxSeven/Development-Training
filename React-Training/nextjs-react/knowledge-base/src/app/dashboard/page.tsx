e728266343445fbd087e5c0fcf42939c91f28365
Samuel Livingston <jinxseven@users.noreply.github.com>
2025-10-21 14:13:27 +0530

    Stats Comp | Dashboard Page

---
+++ React-Training/nextjs-react/knowledge-base/src/app/dashboard/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Stats from "../components/stats";
+import { ArrowUpRight } from "lucide-react";
+import { getUserSession } from "../lib/sessionStorageUtils";
+
+export default function Dashboard() {
+  const user = getUserSession();
+
+  return (
+    <div className="p-6">
+      <div>
+        <h2 className="mb-3 text-3xl font-semibold md:mb-4 md:text-4xl lg:mb-6">
+          Dashboard
+        </h2>
+        <p className="group flex items-center gap-1 text-sm md:text-base lg:text-lg">
+          Email: {user.email}
+        </p>
+        <p className="group flex items-center gap-1 text-sm md:text-base lg:text-lg">
+          Username: {user.username}
+        </p>
+        <p className="group flex items-center gap-1 text-sm md:text-base lg:text-lg">
+          Id: {user.id?.toLocaleUpperCase()}
+        </p>
+      </div>
+      <Stats />
+    </div>
+  );
+}
